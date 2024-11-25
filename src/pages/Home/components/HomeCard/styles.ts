8a9ea69af0d260e5e7259060d65b928fdacd95ec
Áureo Rodrigues <94138315+Aurinho007@users.noreply.github.com>
2024-11-25 20:01:42 -0300

    Update styles.ts

--- src/pages/Home/components/HomeCard/styles.ts
+++ src/pages/Home/components/HomeCard/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   box-shadow: 0px 4px 15px -10px rgba(0,0,0,0.25);
   background-color: ${theme.white};
   border-radius: 10px;
-  padding: 20px;
+  padding: 16px;
   width: 100%;
   max-width: 25%;
   min-width: 315px;
