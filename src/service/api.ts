f56018007db428aff2e532733a1fb359cac57580
Aureo Rodrigues <aurinhorodrigues@gmail.com>
2024-11-25 17:12:48 -0300

    .

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,6 @@
 import { getUser } from "../controllers/userController";
 
-// export const BASE_URL = process.env.REACT_APP_API_URL;
-export const BASE_URL = "http://34.41.102.229:3001/api";
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const EXPIRED_TOKEN_ERROR = "Token expirado";
 
@@ -21,14 +20,11 @@ export const doRequest = async (
     ...(method !== "GET" && { body: JSON.stringify(params) })
   });
 
-  console.table(response);
-
   if (!response.ok) {
     const errorData = await response.json();
     if (errorData.error === "Erro na verificação do token") {
       errorData.error = EXPIRED_TOKEN_ERROR;
     }
-    console.log(errorData);
     throw new Error(errorData.error ?? "Ops! Algo deu errado...");
   }
 
