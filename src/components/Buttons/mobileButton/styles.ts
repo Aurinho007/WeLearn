222e9965f8b4ad38961a400e8bc7fdb56ccafbe6
Aureo Rodrigues <aurinhorodrigues@gmail.com>
2024-11-28 00:15:49 -0300

    .

--- src/components/Buttons/mobileButton/styles.ts
+++ src/components/Buttons/mobileButton/styles.ts
@@ -4,29 +4,30 @@ import Fonts from "../../../assets/fonts/fonts";
 
 export const Container = styled.div`
   position: fixed;
-  width: 92.3svw;
-  padding: 16px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 16px 0;
   bottom: 0;
   left: 0;
   background-color: ${theme.white};
   box-shadow: 0px -4px 15px -10px #666666;
-  z-index: 50;
+  z-index: 99;
 `;
 
 export const PrimaryBtn = styled.button`
   border: none;
-  width: 95%;
+  width: 90%;
   padding: 8px 16px;
   border-radius: 8px;
   font-family: ${Fonts.poppins};
   font-size: 1rem;
-  text-align: center;
   justify-content: center;
   align-items: center;
   background-color: ${theme.blue};
   font-weight: 600;
   color: ${theme.white};
-  margin-bottom: 8px;
+  margin-bottom: 14px;
 
   &:active {
     filter: brightness(0.9);
