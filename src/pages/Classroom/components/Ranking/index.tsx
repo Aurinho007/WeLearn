0a303e0296220348d2595d74686c57c8cce9bdfe
Aureo Rodrigues <aurinhorodrigues@gmail.com>
2024-12-03 21:29:46 -0300

    .

--- src/pages/Classroom/components/Ranking/index.tsx
+++ src/pages/Classroom/components/Ranking/index.tsx
@@ -33,6 +33,9 @@ const Ranking = ({ id }: { id: number }) => {
 
   const getShortName = (fullName: string): string => {
     const nameParts = fullName.trim().split(" ");
+    if(nameParts.length === 1){
+      return nameParts[0];
+    }
     const firstName = nameParts[0];
     const lastName = nameParts[nameParts.length - 1];
 
